a56397cd7132c60ee9f01d123948f1798f8bda4b
ronnie <rondinelibrt18@gmail.com>
2023-10-17 18:14:25 -0300

    add react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import Contexts from './Contexts';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import process from 'process';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Contexts>
-      <App />
-    </Contexts>
+    <BrowserRouter>
+      <Contexts>
+        <App />
+      </Contexts>
+    </BrowserRouter>
   </React.StrictMode>
 );
