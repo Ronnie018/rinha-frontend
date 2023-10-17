a2da887e61fe60fa34f93f0418885856c2342542
ronnie <rondinelibrt18@gmail.com>
2023-10-17 18:08:30 -0300

    add gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import Contexts from './Contexts';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,5 +11,3 @@ root.render(
     </Contexts>
   </React.StrictMode>
 );
-
-reportWebVitals();
