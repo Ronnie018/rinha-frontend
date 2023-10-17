35eeff1bd97c8b37ed22449f6e8621838b1c5de3
ronnie <rondinelibrt18@gmail.com>
2023-10-17 18:15:37 -0300

    add react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import process from 'process';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
